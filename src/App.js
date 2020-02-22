3cdd2cfb3787ba99e7a59ea49ddc544c38a3ccc7
Ben Durrant <ben.j.durrant@gmail.com>
2020-02-22 01:00:01 +0000

    Add more theming, and initial Top App Bar.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DesktopAppBar } from './components/desktopAppBar';
+import { MobileAppBar } from './components/mobileAppBar';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const device = this.props.device;
+    let appBar;
+    if (device === 'desktop') {
+      appBar = <DesktopAppBar />;
+    } else {
+      appBar = <MobileAppBar />;
+    }
+    return (
+      <div className="app">
+        {appBar}
+        <div style={{ height: '100rem', padding: '1rem' }}>Scroll Me</div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+let device;
+
+if (vw >= 840) {
+    device = 'desktop';
+} else if (vw < 840 && vw >= 480) {
+    device = 'tablet';
+} else {
+    device = 'tablet';
+};
+
+console.log(device);
+
+ReactDOM.render(<App device={device}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
