7f0297b8eaff88fc0c50616d8444c2a49917ba4b
Ben Durrant <ben.j.durrant@gmail.com>
2020-04-01 02:16:26 +0100

    Fixed styling of text fields.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -31,7 +31,9 @@ export class SearchBarPersistent extends React.Component {
         return (
             <div className={'search-bar search-bar--persistent' + (this.state.expanded ? ' search-bar--expanded' : '')}>
                 <TextField outlined className="search-bar-field" value={this.props.search} autoComplete="off" placeholder="Search" icon="search" trailingIcon={(this.state.expanded ? ({
-                    icon: 'clear',
+                    icon: (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" opacity=".3"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>
+                        ),
                     tabIndex: 0,
                     onClick: () => this.clearInput()
                 }) : '')} name='search' onChange={this.handleChange} />
@@ -99,7 +101,9 @@ export class SearchBarModal extends React.Component {
         return (
             <div className={'search-bar search-bar--modal' + (this.state.open ? ' search-bar--expanded' : '') + (this.state.opening ? ' search-bar--opening' : '') + (this.state.closing ? ' search-bar--closing' : '') + (this.state.animate ? ' search-bar--animate' : '')}>
                 <TextField outlined className="search-bar-field" value={this.props.search} autoComplete="off" placeholder="Search" icon="search" trailingIcon={{
-                    icon: 'clear',
+                    icon: (
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" opacity=".3"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>
+                    ),
                     tabIndex: 0,
                     onClick: () => this.clearInput()
                 }} name='search' onChange={this.handleChange} />
@@ -137,7 +141,9 @@ export class SearchAppBar extends React.Component {
                     <TopAppBarRow>
                         <div className='search-bar search-bar--modal search-bar--expanded'>
                             <TextField outlined className="search-bar-field" value={this.props.search} autoComplete="off" placeholder="Search" icon="search" trailingIcon={{
-                                icon: 'clear',
+                                icon: (
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" opacity=".3"/><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z"/></svg>
+                                ),
                                 tabIndex: 0,
                                 onClick: () => this.clearInput()
                             }} name='search' onChange={this.handleChange} />
