fec89c02cb3f6b49a4a9ddf7c0e9798d154a098a
Ben Durrant <ben.j.durrant@gmail.com>
2020-10-09 20:27:23 +0100

    Added tooltips to icon-only toggle buttons

--- src/components/util/ToggleGroup.js
+++ src/components/util/ToggleGroup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "@rmwc/button";
+import { Tooltip } from "@rmwc/tooltip";
 import "./ToggleGroup.scss";
 
 export const ToggleGroup = (props) => {
@@ -7,6 +8,29 @@ export const ToggleGroup = (props) => {
 };
 
 export const ToggleGroupButton = (props) => {
+  if (props.tooltip) {
+    return (
+      <Tooltip
+        enterDelay={props.tooltip.enterDelay ? props.tooltip.enterDelay : null}
+        align={props.tooltip.align ? props.tooltip.align : null}
+        content={props.tooltip.content ? props.tooltip.content : null}
+      >
+        <Button
+          outlined
+          label={props.label ? props.label : null}
+          icon={props.icon ? props.icon : null}
+          className={
+            (props.selected
+              ? "mdc-button--selected" + (props.className ? " " + props.className : "")
+              : props.className
+              ? " " + props.className
+              : "") + (props.icon && !props.label ? " only-icon" : "")
+          }
+          onClick={props.onClick}
+        />
+      </Tooltip>
+    );
+  }
   return (
     <Button
       outlined
