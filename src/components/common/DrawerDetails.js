5b71a72b2129fd75ea7914c8eb7ddb7e918d7aba
Ben Durrant <ben.j.durrant@gmail.com>
2020-10-23 20:11:21 +0100

    Made edit and delete buttons equal width.

--- src/components/common/DrawerDetails.js
+++ src/components/common/DrawerDetails.js
@@ -237,15 +237,21 @@ export class DesktopDrawerDetails extends React.Component {
               outlined
               danger
               label="Delete"
+              icon={{
+                strategy: "component",
+                icon: (
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M8 9h8v10H8z" opacity=".3" />
+                    <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z" />
+                  </svg>
+                ),
+              }}
               onClick={() => this.props.delete(this.props.set)}
             />
-          ) : (
-            ""
-          )}
+          ) : null}
         </div>
-      ) : (
-        ""
-      );
+      ) : null;
     const lichButton =
       this.props.set.colorway === "Lich" ? (
         <Ripple unbounded>
@@ -569,15 +575,21 @@ export class TabletDrawerDetails extends React.Component {
               outlined
               danger
               label="Delete"
+              icon={{
+                strategy: "component",
+                icon: (
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M8 9h8v10H8z" opacity=".3" />
+                    <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z" />
+                  </svg>
+                ),
+              }}
               onClick={() => this.props.delete(this.props.set)}
             />
-          ) : (
-            ""
-          )}
+          ) : null}
         </div>
-      ) : (
-        <div></div>
-      );
+      ) : null;
     const lichButton =
       this.props.set.colorway === "Lich" ? (
         <Ripple unbounded>
