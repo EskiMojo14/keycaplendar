635ab99b2b5984e8e306d2771b28dabf22499ad1
Ben Durrant <ben.j.durrant@gmail.com>
2020-11-08 21:24:12 +0000

    Added image preview to sales graph field.

--- src/components/admin/DrawerEntry.js
+++ src/components/admin/DrawerEntry.js
@@ -31,6 +31,7 @@ export class DrawerCreate extends React.Component {
       loading: false,
       imageUploadProgress: 0,
       imageURL: "",
+      salesImageLoaded: false,
       focused: "",
     };
   }
@@ -278,6 +279,10 @@ export class DrawerCreate extends React.Component {
           this.props.snackbarQueue.notify({ title: "Error adding document: " + error });
         });
     }
+  };
+
+  setSalesImageLoaded = (val) => {
+    this.setState({ salesImageLoaded: !!val });
   };
 
   componentDidUpdate(prevProps) {
@@ -649,19 +654,49 @@ export class DrawerCreate extends React.Component {
                 }}
               />
             </div>
-            <TextField
-              autoComplete="off"
-              icon="link"
-              outlined
-              label="Sales"
-              pattern="https?:\/\/.+\.(?:jpg|jpeg|png)"
-              value={this.state.sales}
-              name="sales"
-              helpText={{ persistent: true, validationMsg: true, children: "Must be direct link to image" }}
-              onChange={this.handleChange}
-              onFocus={this.handleFocus}
-              onBlur={this.handleBlur}
-            />
+            <Card outlined className="sales-container">
+              <Typography use="caption" tag="h3" className="sales-title">
+                Sales
+              </Typography>
+              <div className={"sales-image" + (this.state.salesImageLoaded ? " loaded" : "")}>
+                <div className="sales-image-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M10.21 16.83l-1.96-2.36L5.5 18h11l-3.54-4.71z" />
+                    <path
+                      d="M16.5 18h-11l2.75-3.53 1.96 2.36 2.75-3.54L16.5 18zM17 7h-3V6H4v14h14V10h-1V7z"
+                      opacity=".3"
+                    />
+                    <path d="M20 4V1h-2v3h-3v2h3v2.99h2V6h3V4zm-2 16H4V6h10V4H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V10h-2v10z" />
+                  </svg>
+                </div>
+                <img
+                  src={this.state.sales}
+                  alt=""
+                  onLoad={() => {
+                    this.setSalesImageLoaded(true);
+                  }}
+                  onError={() => {
+                    this.setSalesImageLoaded(false);
+                  }}
+                />
+              </div>
+              <div className="sales-field">
+                <TextField
+                  autoComplete="off"
+                  icon="link"
+                  outlined
+                  label="URL"
+                  pattern="https?:\/\/.+"
+                  value={this.state.sales}
+                  name="sales"
+                  helpText={{ persistent: true, validationMsg: true, children: "Must be direct link to image" }}
+                  onChange={this.handleChange}
+                  onFocus={this.handleFocus}
+                  onBlur={this.handleBlur}
+                />
+              </div>
+            </Card>
           </form>
         </DrawerContent>
         <div className="drawer-footer">
@@ -703,6 +738,7 @@ export class DrawerEdit extends React.Component {
       imageUploadProgress: 0,
       imageURL: "",
       newImage: false,
+      salesImageLoaded: false,
       focused: "",
     };
   }
@@ -981,6 +1017,10 @@ export class DrawerEdit extends React.Component {
       .catch((error) => {
         this.props.snackbarQueue.notify({ title: "Error editing document: " + error });
       });
+  };
+
+  setSalesImageLoaded = (val) => {
+    this.setState({ salesImageLoaded: !!val });
   };
 
   render() {
@@ -1332,19 +1372,49 @@ export class DrawerEdit extends React.Component {
                 }}
               />
             </div>
-            <TextField
-              autoComplete="off"
-              icon="link"
-              outlined
-              label="Sales"
-              pattern="https?:\/\/.+\.(?:jpg|jpeg|png)"
-              value={this.state.sales}
-              name="sales"
-              helpText={{ persistent: true, validationMsg: true, children: "Must be direct link to image" }}
-              onChange={this.handleChange}
-              onFocus={this.handleFocus}
-              onBlur={this.handleBlur}
-            />
+            <Card outlined className="sales-container">
+              <Typography use="caption" tag="h3" className="sales-title">
+                Sales
+              </Typography>
+              <div className={"sales-image" + (this.state.salesImageLoaded ? " loaded" : "")}>
+                <div className="sales-image-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M10.21 16.83l-1.96-2.36L5.5 18h11l-3.54-4.71z" />
+                    <path
+                      d="M16.5 18h-11l2.75-3.53 1.96 2.36 2.75-3.54L16.5 18zM17 7h-3V6H4v14h14V10h-1V7z"
+                      opacity=".3"
+                    />
+                    <path d="M20 4V1h-2v3h-3v2h3v2.99h2V6h3V4zm-2 16H4V6h10V4H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V10h-2v10z" />
+                  </svg>
+                </div>
+                <img
+                  src={this.state.sales}
+                  alt=""
+                  onLoad={() => {
+                    this.setSalesImageLoaded(true);
+                  }}
+                  onError={() => {
+                    this.setSalesImageLoaded(false);
+                  }}
+                />
+              </div>
+              <div className="sales-field">
+                <TextField
+                  autoComplete="off"
+                  icon="link"
+                  outlined
+                  label="URL"
+                  pattern="https?:\/\/.+"
+                  value={this.state.sales}
+                  name="sales"
+                  helpText={{ persistent: true, validationMsg: true, children: "Must be direct link to image" }}
+                  onChange={this.handleChange}
+                  onFocus={this.handleFocus}
+                  onBlur={this.handleBlur}
+                />
+              </div>
+            </Card>
           </form>
         </DrawerContent>
         <div className="drawer-footer">
