08d58008dac003e6006e5791862eff80329ac9df
Ben Durrant <ben.j.durrant@gmail.com>
2020-11-23 13:37:52 +0000

    Added ripple to search bar trailing icon.

--- src/components/app_bar/SearchBar.js
+++ src/components/app_bar/SearchBar.js
@@ -99,20 +99,22 @@ export class SearchBarPersistent extends React.Component {
                 ? {
                     icon: (
                       <Ripple unbounded>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
-                          <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
-                          <path
-                            d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
-                            opacity=".3"
-                          />
-                          <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
-                        </svg>
+                        <div className="leading-icon-ripple">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+                            <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+                            <path
+                              d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
+                              opacity=".3"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
+                          </svg>
+                        </div>
                       </Ripple>
                     ),
                     tabIndex: 2,
                     onClick: () => this.clearInput(),
                   }
-                : ""
+                : null
             }
             name="search"
             onChange={this.handleChange}
@@ -270,14 +272,18 @@ export class SearchBarModal extends React.Component {
             }}
             trailingIcon={{
               icon: (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px">
-                  <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
-                  <path
-                    d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
-                    opacity=".3"
-                  />
-                  <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
-                </svg>
+                <Ripple unbounded>
+                  <div className="leading-icon-ripple">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+                      <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+                      <path
+                        d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
+                        opacity=".3"
+                      />
+                      <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
+                    </svg>
+                  </div>
+                </Ripple>
               ),
               tabIndex: 0,
               onClick: () => this.clearInput(),
@@ -383,10 +389,7 @@ export class SearchAppBar extends React.Component {
   render() {
     return (
       <>
-        <TopAppBar
-          fixed
-          className={"search-app-bar" + (this.props.open ? " search-app-bar--open" : "")}
-        >
+        <TopAppBar fixed className={"search-app-bar" + (this.props.open ? " search-app-bar--open" : "")}>
           <TopAppBarRow>
             <div className="search-bar search-bar--modal search-bar--expanded">
               <div className="search-bar-field-container">
@@ -404,20 +407,18 @@ export class SearchAppBar extends React.Component {
                   }}
                   trailingIcon={{
                     icon: (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="black"
-                        width="24px"
-                        height="24px"
-                      >
-                        <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
-                        <path
-                          d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
-                          opacity=".3"
-                        />
-                        <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
-                      </svg>
+                      <Ripple unbounded>
+                        <div className="leading-icon-ripple">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+                            <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+                            <path
+                              d="M12 4c-4.41 0-8 3.59-8 8s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm5 11.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"
+                              opacity=".3"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.59-13L12 10.59 8.41 7 7 8.41 10.59 12 7 15.59 8.41 17 12 13.41 15.59 17 17 15.59 13.41 12 17 8.41z" />
+                          </svg>
+                        </div>
+                      </Ripple>
                     ),
                     tabIndex: 0,
                     onClick: () => this.clearInput(),
