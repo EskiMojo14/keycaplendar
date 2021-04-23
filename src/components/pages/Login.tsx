23bc80e733f9a318e1f087a3a5b21d44995067b9
Ben Durrant <ben.j.durrant@gmail.com>
2021-04-23 13:08:21 +0100

    Enabled Twitter and Github OAuth.

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -20,7 +20,11 @@ export const Login = () => {
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
     signInSuccessUrl: "/",
     // We will display Google and Facebook as auth providers.
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    ],
     callbacks: {
       // Avoid redirects after sign-in.
       signInSuccessWithAuthResult: () => false,
