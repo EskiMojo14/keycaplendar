ab9dba3fd890da54e48511e88a15abe75e8987e7
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-08 20:48:40 +0100

    Created typographyBuilder to easily build a custom component with specified tag and typography setting.

--- src/components/updates/UpdateEntry.tsx
+++ src/components/updates/UpdateEntry.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Card } from "@rmwc/card";
 import { CustomReactMarkdown } from "../util/ReactMarkdown";
+import { Typography } from "@rmwc/typography";
 import "./UpdateEntry.scss";
 
-const testString = "# Hello, *world*!\nMultline **too**?";
+const testString = `Big news! KeycapLendar now supports logins (using Google, Github and/or Twitter authentication), which have features such as:
+
+- Favourites - add a set to your favourites to add it to your favourites page, and you can also filter to only show favorites.
+- Hidden sets - hide a set with the eye icon to hide it from all main pages, and you can of course filter to only show hidden sets.
+- Filter presets - create and save filter presets, and switch between them using a dropdown.
+- Sync settings - choose to sync your app settings (theme, density) to your account, which means that they'll be synced between devices.
+
+[Log in here.](https://keycaplendar.firebaseapp.com/login)`;
 
 export const UpdateEntry = () => {
   return (
     <Card className="update-entry">
+      <Typography use="overline">eskimojo</Typography>
+      <Typography use="headline5">Accounts are now launched!</Typography>
+      <Typography use="caption">21st April 2021</Typography>
       <CustomReactMarkdown>{testString}</CustomReactMarkdown>
     </Card>
   );
--- src/components/util/ReactMarkdown.tsx
+++ src/components/util/ReactMarkdown.tsx
@@ -1,29 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Typography } from "@rmwc/typography";
 import React from "react";
 import ReactMarkdown, { ReactMarkdownOptions } from "react-markdown";
+import { Typography, TypographyT } from "@rmwc/typography";
 
-const h1 = (props: Record<string, any>) => {
-  const { node, children, ...allProps } = props;
-  return (
-    <Typography use="headline5" tag="h1" {...allProps}>
-      {children}
-    </Typography>
-  );
-};
-
-const p = (props: Record<string, any>) => {
-  const { node, children, ...allProps } = props;
-  return (
-    <Typography use="body2" tag="p" {...allProps}>
-      {children}
-    </Typography>
-  );
+const typographyBuilder = (tag: string, typography: TypographyT) => {
+  const component = (props: Record<string, any>) => {
+    const { node, ordered, children, ...allProps } = props;
+    return (
+      <Typography use={typography} tag={tag} {...allProps}>
+        {children}
+      </Typography>
+    );
+  };
+  component.displayName = "Custom " + tag;
+  return component;
 };
 
 const components = {
-  h1: h1,
-  p: p,
+  h1: typographyBuilder("h1", "headline5"),
+  h2: typographyBuilder("h2", "headline6"),
+  h3: typographyBuilder("h3", "subtitle2"),
+  p: typographyBuilder("p", "body2"),
+  li: typographyBuilder("li", "body2"),
 };
 
 type CustomReactMarkdownProps = ReactMarkdownOptions;
