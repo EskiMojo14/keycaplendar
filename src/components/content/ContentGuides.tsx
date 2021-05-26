d34aef9ca66b6087343bcd9573f70d0854fdd3cd
Ben Durrant <ben.j.durrant@gmail.com>
2021-05-26 23:27:22 +0100

    Added FAB and indent to guides page.

--- src/components/content/ContentGuides.tsx
+++ src/components/content/ContentGuides.tsx
@@ -1,8 +1,11 @@
 import classNames from "classnames";
 import React from "react";
 import { useAppSelector } from "../../app/hooks";
+import { selectDevice } from "../../app/slices/common/commonSlice";
 import { pageTitle } from "../../app/slices/common/constants";
 import { selectBottomNav } from "../../app/slices/settings/settingsSlice";
+import { selectUser } from "../../app/slices/user/userSlice";
+import { Fab } from "@rmwc/fab";
 import {
   TopAppBar,
   TopAppBarFixedAdjust,
@@ -18,21 +21,53 @@ type ContentGuidesProps = {
 };
 
 export const ContentGuides = (props: ContentGuidesProps) => {
+  const device = useAppSelector(selectDevice);
+
   const bottomNav = useAppSelector(selectBottomNav);
+
+  const user = useAppSelector(selectUser);
+
+  const indent =
+    user.isAdmin && bottomNav ? (
+      <TopAppBarSection className="indent" alignEnd>
+        <svg xmlns="http://www.w3.org/2000/svg" width="128" height="56" viewBox="0 0 128 56">
+          <path
+            d="M107.3,0a8.042,8.042,0,0,0-7.9,6.6A36.067,36.067,0,0,1,64,36,36.067,36.067,0,0,1,28.6,6.6,8.042,8.042,0,0,0,20.7,0H0V56H128V0Z"
+            fill="inherit"
+          />
+        </svg>
+        <div className="fill"></div>
+      </TopAppBarSection>
+    ) : null;
+
+  const editorElements = user.isAdmin ? (
+    <>
+      <Fab
+        className={classNames("create-fab", { middle: bottomNav })}
+        icon="add"
+        label={device === "desktop" ? "Create" : null}
+      />
+    </>
+  ) : null;
+
   return (
     <>
-      <TopAppBar fixed className={classNames({ "bottom-app-bar": bottomNav })}>
+      <TopAppBar
+        fixed
+        className={classNames({ "bottom-app-bar": bottomNav, "bottom-app-bar--indent": bottomNav && user.isAdmin })}
+      >
         <TopAppBarRow>
           <TopAppBarSection alignStart>
             <TopAppBarNavigationIcon icon="menu" onClick={props.openNav} />
             <TopAppBarTitle>{pageTitle.guides}</TopAppBarTitle>
           </TopAppBarSection>
-          <TopAppBarSection alignEnd></TopAppBarSection>
+          {indent}
         </TopAppBarRow>
       </TopAppBar>
       {bottomNav ? null : <TopAppBarFixedAdjust />}
       <div className="content-container">
         <div className="main extended-app-bar"></div>
+        {editorElements}
       </div>
       <Footer />
       {bottomNav ? <TopAppBarFixedAdjust /> : null}
