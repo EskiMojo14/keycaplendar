2984a278d21b5bd77a633dce06febf18f9b03f95
Ben Durrant <ben.j.durrant@gmail.com>
2021-06-24 21:12:53 +0100

    Use path aliases for pages, settings, statistics and update components.

--- src/components/settings/DialogDelete.tsx
+++ src/components/settings/DialogDelete.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import firebase from "../../app/slices/firebase/firebase";
-import { queue } from "../../app/snackbarQueue";
+import firebase from "@s/firebase/firebase";
+import { queue } from "~/app/snackbarQueue";
 import { Dialog, DialogActions, DialogButton, DialogContent, DialogTitle } from "@rmwc/dialog";
 
 type DialogDeleteProps = {
