d9aa76da014176bfa061fdbcb8c290eed4126567
Ben Durrant <ben.j.durrant@gmail.com>
2021-08-07 18:26:23 +0100

    Added graph toggle for shipped graphs.

--- src/components/statistics/TimelineCard.tsx
+++ src/components/statistics/TimelineCard.tsx
@@ -47,6 +47,8 @@ const listener = { draw: (e: any) => customPoint(e) };
 
 type ShippedCardProps = {
   data: ShippedDataObject;
+  defaultType?: "bar" | "line";
+  overline?: React.ReactNode;
 };
 
 const chartOptions = (monthLabel: string) => {
@@ -124,15 +126,86 @@ const responsiveOptions = [
 
 export const ShippedCard = (props: ShippedCardProps) => {
   const device = useAppSelector(selectDevice);
+  const [graphType, setGraphType] = useState<"bar" | "line">(props.defaultType || "bar");
+  const barChart =
+    graphType === "bar" ? (
+      <ChartistGraph
+        className={device === "desktop" ? "ct-double-octave" : "ct-major-twelfth"}
+        data={{
+          series: [
+            props.data.timeline.series.map((item) => item.shipped),
+            props.data.timeline.series.map((item) => item.unshipped),
+          ],
+          labels: props.data.timeline.months.map((label) => label.split(" ").join("\n")),
+        }}
+        type={"Bar"}
+        options={chartOptions("Month (GB end)")}
+        responsiveOptions={responsiveOptions}
+      />
+    ) : null;
+  const lineChart =
+    graphType === "line" ? (
+      <ChartistGraph
+        className={device === "desktop" ? "ct-double-octave" : "ct-major-twelfth"}
+        data={{
+          series: [
+            props.data.timeline.series.map((item) => item.shipped),
+            props.data.timeline.series.map((item) => item.unshipped),
+          ],
+          labels: props.data.timeline.months.map((label) => label.split(" ").join("\n")),
+        }}
+        type={"Line"}
+        listener={listener}
+        options={chartOptions("Month (GB end)")}
+        responsiveOptions={responsiveOptions}
+      />
+    ) : null;
   return (
     <Card className="timeline-card half-span">
-      <Typography use="headline5" tag="h1">
-        {props.data.name}
-      </Typography>
-      <Typography use="subtitle2" tag="p">
-        {`${props.data.total} set${props.data.total === 1 ? "" : "s"}`}
-      </Typography>
+      <div className="title-container">
+        <div className="text-container">
+          {props.overline ? (
+            <Typography use="overline" tag="h3">
+              {props.overline}
+            </Typography>
+          ) : null}
+          <Typography use="headline5" tag="h1">
+            {props.data.name}
+          </Typography>
+          <Typography use="subtitle2" tag="p">
+            {`${props.data.total} set${props.data.total === 1 ? "" : "s"}`}
+          </Typography>
+        </div>
+        <div className="button-container">
+          <SegmentedButton toggle>
+            {withTooltip(
+              <SegmentedButtonSegment
+                icon="stacked_bar_chart"
+                selected={graphType === "bar"}
+                onClick={() => {
+                  setGraphType("bar");
+                }}
+              />,
+              "Stacked bar chart"
+            )}
+            {withTooltip(
+              <SegmentedButtonSegment
+                icon="multiline_chart"
+                selected={graphType === "line"}
+                onClick={() => {
+                  setGraphType("line");
+                }}
+              />,
+              "Multiline chart"
+            )}
+          </SegmentedButton>
+        </div>
+      </div>
       <div className="timeline-container">
+        <div className="timeline-chart-container shipped">
+          {barChart}
+          {lineChart}
+        </div>
         <div className="table-container">
           <DataTable className="rounded">
             <DataTableContent>
@@ -158,21 +231,6 @@ export const ShippedCard = (props: ShippedCardProps) => {
               </DataTableBody>
             </DataTableContent>
           </DataTable>
-        </div>
-        <div className="timeline-chart-container shipped">
-          <ChartistGraph
-            className={device === "desktop" ? "ct-double-octave" : "ct-major-twelfth"}
-            data={{
-              series: [
-                props.data.timeline.series.map((item) => item.shipped),
-                props.data.timeline.series.map((item) => item.unshipped),
-              ],
-              labels: props.data.timeline.months.map((label) => label.split(" ").join("\n")),
-            }}
-            type={"Bar"}
-            options={chartOptions("Month (GB end)")}
-            responsiveOptions={responsiveOptions}
-          />
         </div>
       </div>
     </Card>
@@ -343,30 +401,26 @@ export const TimelinesCard = (props: TimelinesCardProps) => {
         <div className="button-container">
           {focusButtons}
           <SegmentedButton toggle>
-            <SegmentedButtonSegment
-              icon={iconObject(
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path d="M22,21H2V3H4V19H6V17H10V19H12V16H16V19H18V17H22V21M18,14H22V16H18V14M12,6H16V9H12V6M16,15H12V10H16V15M6,10H10V12H6V10M10,16H6V13H10V16Z" />
-                </svg>
-              )}
-              selected={graphType === "bar"}
-              onClick={() => {
-                setGraphType("bar");
-              }}
-            />
-            <SegmentedButtonSegment
-              icon={iconObject(
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path d="M16,11.78L20.24,4.45L21.97,5.45L16.74,14.5L10.23,10.75L5.46,19H22V21H2V3H4V17.54L9.5,8L16,11.78Z" />
-                </svg>
-              )}
-              selected={graphType === "line"}
-              onClick={() => {
-                setGraphType("line");
-              }}
-            />
+            {withTooltip(
+              <SegmentedButtonSegment
+                icon={props.singleTheme ? "bar_chart" : "stacked_bar_chart"}
+                selected={graphType === "bar"}
+                onClick={() => {
+                  setGraphType("bar");
+                }}
+              />,
+              props.singleTheme ? "Bar chart" : "Stacked bar chart"
+            )}
+            {withTooltip(
+              <SegmentedButtonSegment
+                icon={props.singleTheme ? "show_chart" : "multiline_chart"}
+                selected={graphType === "line"}
+                onClick={() => {
+                  setGraphType("line");
+                }}
+              />,
+              props.singleTheme ? "Line chart" : "Multiline chart"
+            )}
           </SegmentedButton>
         </div>
       </div>
@@ -378,7 +432,7 @@ export const TimelinesCard = (props: TimelinesCardProps) => {
               single: props.singleTheme,
               focused: focused.length > 0,
             },
-            props.singleTheme,
+            typeof props.singleTheme === "string" ? props.singleTheme : "",
             focused.map((index) => `series-index-${index}`)
           )}
         >
