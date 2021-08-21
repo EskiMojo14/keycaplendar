2ef83b0f2a06872eb76f97c3aaa51f065b6e47d5
Ben Durrant <ben.j.durrant@gmail.com>
2021-08-21 16:31:36 +0100

    Added doc comments to Twemoji declarations.

--- types/react-twemoji.d.ts
+++ types/react-twemoji.d.ts
@@ -3,8 +3,20 @@ declare module "react-twemoji" {
   import { ParseObject } from "twemoji";
 
   type TwemojiProps = {
-    noWrapper?: boolean;
+    /** twemoji.parse options. */
     options?: Partial<ParseObject>;
+    /** When `true`, `Twemoji` will not render a wrapping element (with `tag`) to contain children.
+     *
+     * Note that since `twemoji.parse` needs an DOM element reference,
+     * any direct pure text child of `Twemoji` is not parsed when `noWrapper` is `true`.
+     *
+     * e.g. `foo` in `<Twemoji noWrapper={true}>foo<p>bar</p></Twmoji>` is not parsed.
+     */
+    noWrapper?: boolean;
+    /** The tag of the wrapping element. `div` by default.
+     *
+     * This option is ignored when `noWrapper` is `true`.
+     * */
     tag?: string;
   };
 
