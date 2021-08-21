36e0646eca1828f2172539b520e4550c05e6c883
Ben Durrant <ben.j.durrant@gmail.com>
2021-08-21 20:08:31 +0100

    Create store initial state object for easy merges.

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -4,12 +4,29 @@ import { initialState as common } from "@s/common";
 import { initialState as guides } from "@s/guides";
 import { initialState as history } from "@s/history";
 import { initialState as images } from "@s/images";
+import { initialState as main } from "@s/main";
+import { initialState as settings } from "@s/settings";
 import { initialState as statistics } from "@s/statistics";
 import { initialState as updates } from "@s/updates";
+import { initialState as user } from "@s/user";
 import { initialState as users } from "@s/users";
 
+export const storeInitialState = {
+  audit,
+  common,
+  guides,
+  history,
+  images,
+  main,
+  settings,
+  statistics,
+  updates,
+  user,
+  users,
+};
+
 export const hydrateState = (state: any) => {
-  return { ...state, audit, common, guides, history, images, statistics, updates, users };
+  return { ...storeInitialState, ...state };
 };
 
 export const loadState = () => {
