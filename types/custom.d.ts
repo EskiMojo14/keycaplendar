0f0da68fa47730604dde10be75ef6bc8461d0955
Ben Durrant <ben.j.durrant@gmail.com>
2021-08-21 20:13:26 +0100

    Remove unnecessary SVG type declarations.

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,10 +1,3 @@
-declare module "*.svg" {
-  import { ComponentType, SVGProps } from "react";
-  export const ReactComponent: ComponentType<SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
-
 declare module "csstype" {
   interface Properties {
     "--animation-delay": number;
