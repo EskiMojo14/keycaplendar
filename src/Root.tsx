ecda42fc8572a957c494ca4d18ee211bcf6f3088
Ben Durrant <ben.j.durrant@gmail.com>
2021-08-24 21:53:47 +0100

    Use localeString for picker toolbar.

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,14 +3,21 @@ import { Provider } from "react-redux";
 import { RMWCProvider } from "@rmwc/provider";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import LuxonUtils from "@date-io/luxon";
+import { DateTime } from "luxon";
 import App from "./App";
 import store from "~/app/store";
 import { ThemeProvider } from "@c/util/ThemeProvider";
 
+class LocalizedUtils extends LuxonUtils {
+  getDatePickerHeaderText(date: DateTime) {
+    return date.toLocaleString({ weekday: "short", month: "short", day: "numeric" });
+  }
+}
+
 export const Root = () => {
   return (
     <Provider store={store}>
-      <MuiPickersUtilsProvider utils={LuxonUtils}>
+      <MuiPickersUtilsProvider utils={LocalizedUtils}>
         <ThemeProvider>
           <RMWCProvider tooltip={{ enterDelay: 500, align: "bottom" }}>
             <App />
