896e99fe08bba5a51e33cc509c865612fef44a49
Ben Durrant <ben.j.durrant@gmail.com>
2021-10-16 16:02:48 +0100

    Use icon components for image components.

--- src/components/images/index.tsx
+++ src/components/images/index.tsx
@@ -57,6 +57,7 @@ import { withTooltip } from "@c/util/hocs";
 import { DrawerDetails } from "./drawer-details";
 import { DrawerSearch } from "./drawer-search";
 import { DialogDelete } from "./dialog-delete";
+import { Delete, PermMedia } from "@i";
 import "./index.scss";
 
 const storage = firebase.storage();
@@ -217,18 +218,7 @@ export const ContentImages = (props: ContentImagesProps) => {
             {contextual ? (
               <>
                 {withTooltip(
-                  <TopAppBarActionItem
-                    icon={iconObject(
-                      <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                          <path d="M0 0h24v24H0V0z" fill="none" />
-                          <path d="M8 9h8v10H8z" opacity=".3" />
-                          <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z" />
-                        </svg>
-                      </div>
-                    )}
-                    onClick={openDelete}
-                  />,
+                  <TopAppBarActionItem icon={iconObject(<Delete />, {}, true)} onClick={openDelete} />,
                   "Delete",
                   { align: tooltipAlign }
                 )}
@@ -241,21 +231,7 @@ export const ContentImages = (props: ContentImagesProps) => {
                 {device === "mobile" ? (
                   <MenuSurfaceAnchor>
                     {withTooltip(
-                      <TopAppBarActionItem
-                        icon={iconObject(
-                          <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                              <path d="M0 0h24v24H0V0z" fill="none" />
-                              <path
-                                d="M13.17 6l-.59-.59L11.17 4H6v12h16V6h-8.83zm4.33 4.5L21 15H7l4.5-6 3.5 4.51 2.5-3.01z"
-                                opacity=".3"
-                              />
-                              <path d="M2 6H0v5h.01L0 20c0 1.1.9 2 2 2h18v-2H2V6zm5 9h14l-3.5-4.5-2.5 3.01L11.5 9zM22 4h-8l-2-2H6c-1.1 0-1.99.9-1.99 2L4 16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 12H6V4h5.17l1.41 1.41.59.59H22v10z" />
-                            </svg>
-                          </div>
-                        )}
-                        onClick={openFolderMenu}
-                      />,
+                      <TopAppBarActionItem icon={iconObject(<PermMedia />, {}, true)} onClick={openFolderMenu} />,
                       "Folder",
                       { align: tooltipAlign }
                     )}
