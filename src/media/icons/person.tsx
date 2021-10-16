8ad5db44501af143360c85ed65e63b5127437946
Ben Durrant <ben.j.durrant@gmail.com>
2021-10-16 14:31:46 +0100

    Use SVG components for nav drawer and footer.

---
+++ src/media/icons/person.tsx
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+const SvgPerson = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M12 16c-2.69 0-5.77 1.28-6 2h12c-.2-.71-3.3-2-6-2z" opacity={0.3} />
+    <circle cx={12} cy={8} opacity={0.3} r={2} />
+    <path d="M12 14c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm-6 4c.22-.72 3.31-2 6-2 2.7 0 5.8 1.29 6 2H6zm6-6c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z" />
+  </svg>
+);
+
+const MemoSvgPerson = memo(SvgPerson);
+export default MemoSvgPerson;
