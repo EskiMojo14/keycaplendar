76ed69efdc525f6d62be2ffa38d7844d9ac87c70
Ben Durrant <ben.j.durrant@gmail.com>
2021-10-16 19:08:19 +0100

    Use icon components for more components.

--- src/components/util/pickers/date-picker.tsx
+++ src/components/util/pickers/date-picker.tsx
@@ -14,6 +14,7 @@ import { TextField, TextFieldHelperTextProps, TextFieldHTMLProps, TextFieldProps
 import { KeyboardDatePicker, KeyboardDatePickerProps } from "@material-ui/pickers";
 import { ConditionalWrapper } from "@c/util/conditional-wrapper";
 import { withTooltip } from "@c/util/hocs";
+import { Event } from "@i";
 import "./pickers.scss";
 
 const parseDigits = (string: string) => (string.match(/\d+/g) || []).join("");
@@ -260,21 +261,7 @@ export const DatePicker = ({
         trailingIcon={
           useInline
             ? undefined
-            : withTooltip(
-                <IconButton
-                  icon={iconObject(
-                    <div>
-                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M5 8h14V6H5z" opacity=".3" />
-                        <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5z" />
-                      </svg>
-                    </div>
-                  )}
-                  onClick={() => setOpen(true)}
-                />,
-                "Date picker"
-              )
+            : withTooltip(<IconButton icon={iconObject(<Event />)} onClick={() => setOpen(true)} />, "Date picker")
         }
       />
       {modal}
