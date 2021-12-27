ff6276c661d954897ebe5038f5a582b4774e7f02
Ben Durrant <ben.j.durrant@gmail.com>
2021-12-27 13:51:59 +0000

    Add eslint-plugin import.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: [
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -16,8 +18,7 @@ module.exports = {
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
+    "import/no-named-as-default": "off",
     "object-shorthand": "error",
     "prefer-destructuring": [
       "error",
@@ -29,10 +30,20 @@ module.exports = {
         enforceForRenamedProperties: true,
       },
     ],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
     },
   },
 };
