4b06b9996bcf7afa1b36629d6fe207f877fe2dc0
Ben Durrant <ben.j.durrant@gmail.com>
2021-12-28 18:15:23 +0000

    Remove js override.

--- .eslintrc.js
+++ .eslintrc.js
@@ -124,7 +124,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["*.js", "*.ts", "*.tsx"], // Your TypeScript files extension
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
       parserOptions: {
         project: ["./tsconfig.json"], // Specify it only for TypeScript files
       },
