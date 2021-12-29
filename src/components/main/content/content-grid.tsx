eb4c47361ccd1a8e800c42f45bf8f16ce3d2f3b9
Ben Durrant <ben.j.durrant@gmail.com>
2021-12-29 17:59:59 +0000

    Create basic skeleton block component.

--- src/components/main/content/content-grid.tsx
+++ src/components/main/content/content-grid.tsx
@@ -4,7 +4,8 @@ import { ViewCard } from "@c/main/views/card/view-card";
 import { ViewCompact } from "@c/main/views/compact/view-compact";
 import { ViewImageList } from "@c/main/views/image-list/view-image-list";
 import { ViewList } from "@c/main/views/list/view-list";
-import { selectSetGroups } from "@s/main";
+import { SkeletonBlock } from "@c/util/skeleton-block";
+import { selectLoading, selectSetGroups } from "@s/main";
 import type { SetType } from "@s/main/types";
 import { selectView } from "@s/settings";
 import "./content-grid.scss";
@@ -16,44 +17,21 @@ type ContentGridProps = {
   edit: (set: SetType) => void;
 };
 
-export const ContentGrid = (props: ContentGridProps) => {
+export const ContentGrid = ({ details, closeDetails, detailSet, edit }: ContentGridProps) => {
   const view = useAppSelector(selectView);
 
   const setGroups = useAppSelector(selectSetGroups);
+  const loading = useAppSelector(selectLoading);
 
   const createGroup = (sets: SetType[]) => {
     if (view === "card") {
-      return (
-        <ViewCard
-          sets={sets}
-          details={props.details}
-          closeDetails={props.closeDetails}
-          detailSet={props.detailSet}
-          edit={props.edit}
-        />
-      );
+      return <ViewCard {...{ sets, details, detailSet, closeDetails, edit }} />;
     } else if (view === "list") {
-      return (
-        <ViewList sets={sets} details={props.details} closeDetails={props.closeDetails} detailSet={props.detailSet} />
-      );
+      return <ViewList {...{ sets, details, detailSet, closeDetails, edit }} />;
     } else if (view === "imageList") {
-      return (
-        <ViewImageList
-          sets={sets}
-          details={props.details}
-          closeDetails={props.closeDetails}
-          detailSet={props.detailSet}
-        />
-      );
+      return <ViewImageList {...{ sets, details, detailSet, closeDetails, edit }} />;
     } else if (view === "compact") {
-      return (
-        <ViewCompact
-          sets={sets}
-          details={props.details}
-          closeDetails={props.closeDetails}
-          detailSet={props.detailSet}
-        />
-      );
+      return <ViewCompact {...{ sets, details, detailSet, closeDetails, edit }} />;
     }
   };
   return (
@@ -62,9 +40,13 @@ export const ContentGrid = (props: ContentGridProps) => {
         return (
           <div className="outer-container" key={group.title}>
             <div className="subheader">
-              <Typography use="caption">
-                {group.title} <b>{`(${group.sets.length})`}</b>
-              </Typography>
+              {loading ? (
+                <SkeletonBlock typography="caption" content={`${group.title} (${group.sets.length})`} />
+              ) : (
+                <Typography use="caption">
+                  {group.title} <b>{`(${group.sets.length})`}</b>
+                </Typography>
+              )}
             </div>
             {createGroup(group.sets)}
           </div>
---
+++ src/components/util/skeleton-block.tsx
@@ -0,0 +1,34 @@
+import type { HTMLAttributes } from "react";
+import type { TypographyT } from "@rmwc/typography";
+import BEMHelper from "@s/common/bem-helper";
+import "./skeleton-block.scss";
+
+export type SkeletonBlockProps = Omit<HTMLAttributes<HTMLDivElement>, "children"> & {
+  width?: number | string;
+  height?: number | string;
+  typography?: TypographyT;
+  content?: string;
+};
+
+const bemClasses = new BEMHelper("skeleton-block");
+
+export const SkeletonBlock = ({
+  height,
+  width,
+  typography,
+  content,
+  className = "",
+  style = {},
+  ...props
+}: SkeletonBlockProps) => (
+  <div
+    {...props}
+    className={bemClasses({
+      modifiers: {
+        typography: !!typography,
+      },
+      extra: { [className]: !!className, [`mdc-typography--${typography}`]: !!typography },
+    })}
+    style={{ ...style, height, width, [`--content`]: `"${content}"` }}
+  />
+);
