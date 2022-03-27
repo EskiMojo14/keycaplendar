4ef04c885d04bcd106aa063f14b2a2aa5e53d298
Ben Durrant <ben.j.durrant@gmail.com>
2022-03-27 21:25:01 +0100

    Memoise predicate using createSelector (thanks @phryneas)

--- src/app/local-storage.ts
+++ src/app/local-storage.ts
@@ -1,23 +1,31 @@
+import type { AnyAction } from "@reduxjs/toolkit";
+import { createSelector } from "@reduxjs/toolkit";
 import pick from "lodash.pick";
 import type { RootState } from "~/app/store";
 import { initialState as common } from "@s/common";
 import { initialState as main } from "@s/main";
 import { selectCookies } from "@s/settings";
-import { hasKey, objectEntries, objectFromEntries } from "@s/util/functions";
+import { objectEntries, objectFromEntries } from "@s/util/functions";
 import type { ObjectEntries } from "@s/util/types";
 import type { AppStartListening } from "~/app/middleware/listener";
 
 const initialStates: Partial<RootState> = { common, main };
 
-const idPersistWhitelist = <
-  Whitelist extends {
-    [K in keyof RootState]?:
-      | true
-      | [keyof RootState[K], ...(keyof RootState[K])[]];
-  }
->(
-  whitelist: Whitelist
-) => whitelist;
+type WhitelistDef = {
+  [K in keyof RootState]?:
+    | true
+    | [keyof RootState[K], ...(keyof RootState[K])[]];
+};
+
+type WhitelistedState<Whitelist extends WhitelistDef> = {
+  [Key in keyof RootState as Key extends keyof Whitelist
+    ? Key
+    : never]: Whitelist[Key] extends (keyof RootState[Key])[]
+    ? Pick<RootState[Key], Whitelist[Key][number]>
+    : RootState[Key];
+};
+
+const idPersistWhitelist = <W extends WhitelistDef>(whitelist: W) => whitelist;
 
 const persistWhitelist = idPersistWhitelist({
   common: ["theme", "themeMaps"],
@@ -38,13 +46,7 @@ const persistWhitelist = idPersistWhitelist({
 
 type PersistWhitelist = typeof persistWhitelist;
 
-type WhitelistedState = {
-  [Key in keyof PersistWhitelist]: PersistWhitelist[Key] extends (keyof RootState[Key])[]
-    ? Pick<RootState[Key], PersistWhitelist[Key][number]>
-    : RootState[Key];
-};
-
-export const hydrateState = (state: WhitelistedState) =>
+export const hydrateState = (state: WhitelistedState<PersistWhitelist>) =>
   objectFromEntries(
     objectEntries(persistWhitelist).map(([key, whitelist]) => [
       key,
@@ -72,7 +74,7 @@ export const sanitiseState = (state: RootState) =>
     objectEntries(persistWhitelist).map(([key, whitelist]) => [
       key,
       Array.isArray(whitelist) ? pick(state[key], ...whitelist) : state[key],
-    ]) as ObjectEntries<{ [Key in keyof RootState]?: Partial<RootState[Key]> }>
+    ]) as ObjectEntries<WhitelistedState<PersistWhitelist>>
   );
 
 export const saveState = (state: RootState) => {
@@ -87,6 +89,35 @@ export const saveState = (state: RootState) => {
   }
 };
 
+/**
+ * Creates a selector that checks the current state and old state for a specified key,
+ * (or a specified key's specified keys if `whitelist` is an array)
+ * and returns whether everything we care about is the same
+ */
+const createSubStateComparator = <K extends keyof PersistWhitelist>([
+  key,
+  whitelist,
+]: [K, PersistWhitelist[K]]) =>
+  createSelector(
+    (_: AnyAction, currentState: RootState) => currentState[key],
+    (_: unknown, __: unknown, oldState: RootState) => oldState[key],
+    (current, old) =>
+      current === old || // if we've reached this point, something inside must have changed
+      (Array.isArray(whitelist) && // if whitelist was `true`, we've already returned `false` because we only care about if *something* changed
+        // now we check whether any of the bits we care about have changed
+        /* @ts-expect-error typescript doesn't like that the tuples don't intersect, and i don't want to have to prove that the keys work, we've already checked that when making the whitelist */
+        whitelist.every((subKey) => current[subKey] === old[subKey]))
+  );
+
+/**
+ * Turn each of our whitelist entries into a selector, then check whether they all return true (i.e nothing has changed)
+ */
+
+const predicate = createSelector(
+  ...objectEntries(persistWhitelist).map(createSubStateComparator),
+  (...allEquals: boolean[]) => !allEquals.every(Boolean)
+);
+
 export const setupPersistListener = (startListening: AppStartListening) =>
   startListening({
     effect: async (action, { cancelActiveListeners, delay, getState }) => {
@@ -96,14 +127,5 @@ export const setupPersistListener = (startListening: AppStartListening) =>
 
       saveState(getState());
     },
-    predicate: (action, currentState, originalState) =>
-      objectEntries(persistWhitelist).some(([key, whitelist]) =>
-        Array.isArray(whitelist)
-          ? whitelist.some(
-              (innerKey) =>
-                hasKey(currentState[key], innerKey) &&
-                currentState[key][innerKey] !== originalState[key][innerKey]
-            )
-          : currentState[key] !== originalState[key]
-      ),
+    predicate,
   });
