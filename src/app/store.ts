897ea5896844ee27693ee18ae1dcd796f3d925a4
Ben Durrant <ben.j.durrant@gmail.com>
2022-04-04 21:22:58 +0100

    Add redux batch enhancer.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,11 @@
+import { reduxBatch } from "@manaflair/redux-batch";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import type { AnyAction, ThunkAction } from "@reduxjs/toolkit";
+import type {
+  Action,
+  AnyAction,
+  ThunkAction,
+  UnsubscribeListener,
+} from "@reduxjs/toolkit";
 import { loadState } from "~/app/local-storage";
 import audit from "@s/audit";
 import common from "@s/common";
@@ -32,8 +38,16 @@ const reducer = combineReducers(reducers);
 
 export type RootState = ReturnType<typeof reducer>;
 
-export const createStore = (preloadedState?: Partial<RootState>) =>
+export type AppThunk<ReturnType> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AnyAction
+>;
+
+const _createStore = (preloadedState?: Partial<RootState>) =>
   configureStore({
+    enhancers: (enhancers) => [reduxBatch, ...enhancers, reduxBatch],
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         immutableCheck: false,
@@ -43,16 +57,33 @@ export const createStore = (preloadedState?: Partial<RootState>) =>
     reducer,
   });
 
+type BatchTuple = [AnyAction | AppThunk<any>, ...(AnyAction | AppThunk<any>)[]];
+
+type DispatchActionReturn<DispatchedAction> = DispatchedAction extends AppThunk<
+  infer ThunkReturn
+>
+  ? ThunkReturn
+  : DispatchedAction extends Action<"listenerMiddleware/add">
+  ? UnsubscribeListener
+  : DispatchedAction;
+
+type MapTupleToDispatchReturn<Actions extends BatchTuple> = {
+  [Index in keyof Actions]: DispatchActionReturn<Actions[Index]>;
+} & { length: Actions["length"] };
+
+const createStore: (...params: Parameters<typeof _createStore>) => ReturnType<
+  typeof _createStore
+> & {
+  dispatch: {
+    <Actions extends BatchTuple>(
+      actions: Actions
+    ): MapTupleToDispatchReturn<Actions>;
+  };
+} = _createStore;
+
 export type AppStore = ReturnType<typeof createStore>;
 
 export type AppDispatch = AppStore["dispatch"];
-
-export type AppThunk<ReturnType> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  AnyAction
->;
 
 export const store = createStore(loadState());
 
