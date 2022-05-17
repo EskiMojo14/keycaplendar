c5da451d83cd886a212dc1fb3772a7ca0330c04f
Ben Durrant <ben.j.durrant@gmail.com>
2022-05-17 22:50:25 +0100

    Export new theme.

--- gulp/index.ts
+++ gulp/index.ts
@@ -1,35 +1,41 @@
 import fs from "fs";
+import { parse, stringify } from "comment-json";
+import { camelCase, cloneDeep } from "lodash";
 import prettier from "prettier";
 import sass from "sass";
-import yargs from "yargs/yargs";
-import { camelCase, cloneDeep } from "lodash";
 import cssToJS from "transform-css-to-js";
-import { parse, stringify } from "comment-json";
+import yargs from "yargs/yargs";
 
 const { p: format } = yargs(process.argv.slice(2))
   .options({
-    p: { type: "boolean", alias: ["format", "prettier"], default: false },
+    p: { alias: ["format", "prettier"], default: false, type: "boolean" },
   })
   .parse();
 
-const objectEditDeep = <InputVal = string, OutputVal = string>(
-  keyCB: (string: string) => string = (string) => string,
-  valCB: (V: InputVal) => OutputVal = (val) => (val as unknown) as OutputVal
-) => <T extends Record<string, InputVal | OutputVal>, Nested extends T | T[] | Record<string, T>>(
-  input: Nested
-): Nested => {
-  if (typeof input !== "object") return input;
-  if (Array.isArray(input)) return input.map(objectEditDeep(keyCB, valCB)) as Nested;
-  return Object.fromEntries(
-    Object.entries(input).map(([k, v]) => [
-      keyCB(k),
-      typeof v === "object" ? objectEditDeep(keyCB, valCB)(v) : valCB(v),
-    ])
-  ) as Nested;
-};
+const objectEditDeep =
+  <InputVal = string, OutputVal = string>(
+    keyCB: (string: string) => string = (string) => string,
+    valCB: (V: InputVal) => OutputVal = (val) => val as unknown as OutputVal
+  ) =>
+  <
+    T extends Record<string, InputVal | OutputVal>,
+    Nested extends Record<string, T> | T | T[]
+  >(
+    input: Nested
+  ): Nested => {
+    if (typeof input !== "object") return input;
+    if (Array.isArray(input))
+      return input.map(objectEditDeep(keyCB, valCB)) as Nested;
+    return Object.fromEntries(
+      Object.entries(input).map(([k, v]) => [
+        keyCB(k),
+        typeof v === "object" ? objectEditDeep(keyCB, valCB)(v) : valCB(v),
+      ])
+    ) as Nested;
+  };
 
 const handleLists = (object: any): any =>
-  Object.entries(object).reduce((prev, [mapName, mapVal]) => {
+  Object.entries(object).reduce<any>((prev, [mapName, mapVal]) => {
     const [map, theme] = mapName.split("__");
     const copy = cloneDeep(prev);
     if (typeof copy[map] === "undefined") {
@@ -43,41 +49,58 @@ const handleLists = (object: any): any =>
           copy[map][theme] = {};
         }
         const keyIndexRegex = /(.*)--(\d+)/;
-        Object.entries(mapVal as Record<string, unknown>).forEach(([keyIndex, val]) => {
-          if (typeof copy[map][theme] === "object") {
-            if (keyIndexRegex.test(keyIndex)) {
-              const [_full, key, index] = keyIndex.match(keyIndexRegex) as Array<string>;
-              if (!(copy[map][theme][key] instanceof Array)) {
-                copy[map][theme][key] = [];
+        Object.entries(mapVal as Record<string, unknown>).forEach(
+          ([keyIndex, val]) => {
+            if (typeof copy[map][theme] === "object") {
+              if (keyIndexRegex.test(keyIndex)) {
+                const [_full, key, index] = keyIndex.match(
+                  keyIndexRegex
+                ) as string[];
+                if (!(copy[map][theme][key] instanceof Array)) {
+                  copy[map][theme][key] = [];
+                }
+                copy[map][theme][key][parseInt(index) - 1] = val;
+              } else {
+                copy[map][theme][keyIndex] = val;
               }
-              copy[map][theme][key][parseInt(index) - 1] = val;
-            } else {
-              copy[map][theme][keyIndex] = val;
             }
           }
-        });
+        );
       }
     }
     return copy;
-  }, {} as any);
+  }, {});
 
 export const exportSCSS = (cb: () => void) => {
-  const options: sass.Options = {
+  const options: sass.LegacyOptions<"sync"> = {
     file: "src/export.scss",
     includePaths: ["src/", "node_modules/"],
   };
   const result = sass.renderSync(options);
   const css = result.css.toString();
-  const jsonString = cssToJS(css).replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
+  const jsonString = cssToJS(css).replace(
+    /(['"])?([a-zA-Z0-9_]+)(['"])?:/g,
+    '"$2": '
+  );
   const parsed = parse(jsonString);
   const editKey = (key: string) =>
     key
       .replace(/Element$/, "")
-      .replace(/(graphColors|themesMap)(.*)/, (_full, group, name) => `${group}__${camelCase(name)}`)
-      .replace(/ARRAY(.*)INDEX/g, (_full, name) => `ARRAY${camelCase(name)}INDEX`)
+      .replace(
+        /(graphColors|themesMap)(.*)/,
+        (_full, group, name) => `${group}__${camelCase(name)}`
+      )
+      .replace(
+        /ARRAY(.*)INDEX/g,
+        (_full, name) => `ARRAY${camelCase(name)}INDEX`
+      )
       .replace(/ARRAY(.*)INDEX(\d+)/, "$1--$2");
   const editValue = (value: string) =>
-    value === "undefined" ? "" : ["true", "false"].includes(value) ? value === "true" : value;
+    value === "undefined"
+      ? ""
+      : ["true", "false"].includes(value)
+      ? value === "true"
+      : value;
   const processedParsed = objectEditDeep(editKey, editValue)(parsed);
   const listProcessed = handleLists(processedParsed);
   const writeFile = (data: string) => {
@@ -89,7 +112,12 @@ export const exportSCSS = (cb: () => void) => {
       .resolveConfig("src/theme-variables.json")
       .then((options) => {
         if (options) {
-          writeFile(prettier.format(stringify(listProcessed), { ...options, parser: "json" }));
+          writeFile(
+            prettier.format(stringify(listProcessed), {
+              ...options,
+              parser: "json",
+            })
+          );
         } else {
           writeFile(stringify(listProcessed));
         }
